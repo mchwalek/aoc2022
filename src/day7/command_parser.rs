8758e0a93c4557c9b81f80c209f868b0654d6638
Maksymilian Chwałek <mchwalek89@gmail.com>
2023-10-27 20:42:37 +0200

    Add basic ls parser

--- src/day7/command_parser.rs
+++ src/day7/command_parser.rs
@@ -1,4 +1,4 @@
-use std::iter;
+use std::iter::{self, Peekable};
 
 use super::file_system::FileSystem;
 
@@ -8,9 +8,9 @@ pub struct CommandParser {
 impl CommandParser {
     const COMMAND_LINE_PREFIX: &'static str = "$ ";
 
-    pub fn parse<'a, 'b, T: Iterator<Item = String>>(iterator: &mut T) -> Result<Command, String> {
+    pub fn parse<'a, 'b, T: Iterator<Item = String>>(iterator: &mut Peekable<T>) -> Result<Command, String> {
         let line = iterator.next().ok_or_else(|| "empty iterator passed".to_string())?;
-        if !line.starts_with(Self::COMMAND_LINE_PREFIX) {
+        if !Self::command_line(&line) {
             return Err(format!("not a command line: '{}'", line));
         }
 
@@ -26,16 +26,35 @@ impl CommandParser {
 
                 Ok(Command::Cd { dir: args[0].to_string() })
             },
-            "ls" => { todo!() },
+            "ls" => {
+                let mut output: Vec<String> = Vec::new();
+                while !Self::on_last_output_line(iterator) {
+                    let output_line = iterator.next().unwrap();
+                    output.push(output_line);
+                }
+
+                Ok(Command::Ls { output })
+            },
             _ => Err(format!("invalid command '{}' in line '{}'", name, line)),
+        }
+    }
+
+    fn command_line(line: &str) -> bool {
+        line.starts_with(Self::COMMAND_LINE_PREFIX)
+    }
+
+    fn on_last_output_line<T: Iterator<Item = String>>(iterator: &mut Peekable<T>) -> bool {
+        match iterator.peek() {
+            Some(line) => Self::command_line(line),
+            None => true,
         }
     }
 }
 
 #[derive(PartialEq, Debug)]
 pub enum Command {
+    Cd { dir: String },
     Ls { output: Vec<String> },
-    Cd { dir: String }
 }
 
 impl Command {
@@ -52,18 +71,26 @@ mod tests {
     use crate::day7::command_parser::*;
 
     #[test]
-    fn returns_ls_command() {
-    }
-
-    #[test]
     fn returns_cd_command() {
         let mut iter = command_string_to_iter("$ cd dir".to_string());
         assert_eq!(Ok(Command::Cd { dir: "dir".to_string() }), CommandParser::parse(&mut iter));
     }
 
     #[test]
+    fn returns_ls_command() {
+        let mut iter = vec![
+            "$ ls".to_string(),
+            "dir a".to_string(),
+            "123 b.txt".to_string()
+        ].into_iter().peekable();
+        assert_eq!(
+            Ok(Command::Ls { output: vec!["dir a".to_string(), "123 b.txt".to_string()] }),
+            CommandParser::parse(&mut iter));
+    }
+
+    #[test]
     fn handles_general_errors() {
-        let mut empty_iter = iter::empty::<String>();
+        let mut empty_iter = iter::empty::<String>().peekable();
         assert_eq!(Err("empty iterator passed".to_string()), CommandParser::parse(&mut empty_iter));
 
         let mut iter = command_string_to_iter("cd dir".to_string());
@@ -85,7 +112,7 @@ mod tests {
         assert_eq!(Err("expected exactly 1 param in line '$ cd dir dir2'".to_string()), CommandParser::parse(&mut iter));
     }
 
-    fn command_string_to_iter(command: String) -> impl Iterator<Item = String> {
-        vec![command].into_iter()
+    fn command_string_to_iter(command: String) -> Peekable<impl Iterator<Item = String>> {
+        vec![command].into_iter().peekable()
     }
 }
