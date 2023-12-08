3c7bfeeaf465881be6ae350377d359a2b6684e91
Maksymilian Chwałek <mchwalek89@gmail.com>
2023-12-08 21:10:06 +0100

    Day 8 get solution

--- src/day8.rs
+++ src/day8.rs
@@ -7,16 +7,23 @@ use std::{
 
 use self::tree_grid::TreeGrid;
 
-pub fn run(path: &str) -> usize {
+pub fn run(path: &str) -> (usize, usize) {
     let file = File::open(path).unwrap();
     let reader = io::BufReader::new(file);
     let lines_iter = reader.lines().map(|x| x.unwrap());
 
     let tree_grid = TreeGrid::parse(lines_iter).unwrap();
-    tree_grid
+    let visible_from_outside_count = tree_grid
         .tree_iter()
         .filter(|x| tree_grid.tree_visible(x))
-        .count()
+        .count();
+    let highest_scenic_score = tree_grid
+       .tree_iter()
+       .map(|x| tree_grid.scenic_score(x))
+       .max()
+       .unwrap();
+
+    (visible_from_outside_count, highest_scenic_score)
 }
 
 #[cfg(test)]
